8d056f8206507f3100fac7745bb14a00e4ac1ebd
SonSon-CG <son.mup@gmail.com>
2025-01-09 18:27:23 +0700

    add-car-ctrl

--- Quan-ly-ban-xe/src/controller/CarController.java
+++ Quan-ly-ban-xe/src/controller/CarController.java
@@ -4,8 +4,29 @@ import model.Car;
 import model.Inventory;
 import model.Sedan;
 
+import java.io.*;
+import java.util.List;
+
 public class CarController {
     Car sedan1 = new Sedan("30A-00000","Toyota", 15000, 4);
     Car sedan2 = new Sedan("30A-11111","Toyota", 12000, 2);
 private Inventory inventory = Inventory.getInstance();
+
+    public void saveInventoryToFile(String fileName) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            oos.writeObject(inventory.getCars());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadInventoryFromFile(String fileName) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+            inventory.getCars().clear();
+            inventory.getCars().addAll((List<Car>) ois.readObject());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
